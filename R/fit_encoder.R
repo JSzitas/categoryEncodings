2abfdf511b325ede16c5d04f93e4885be66a004e
Juraj Szitas <szitas.juraj13@gmail.com>
2021-09-26 00:11:49 +0200

    Add de-encoding function

--- R/fit_encoder.R
+++ R/fit_encoder.R
@@ -31,6 +31,34 @@ fit_encoder <- function( X, encodings ) {
     }
     return( res )
   }
+  encoder_reverse <- function( X ) {
+    X <- to_data_table(X)
+    
+    all_encoded_names <- NULL
+    encoded_columns <- colnames( recorded_encodings[[1]] )
+    # the factor name always comes first, so the join is always on names other 
+    # than the first name
+    encoded_columns <- encoded_columns[ 2:length(encoded_columns) ]
+    res <- recorded_encodings[[1]][X, on = encoded_columns ]
+    all_encoded_names <- c( all_encoded_names, encoded_columns)
+    
+    if (length(encoding_names) > 1) {
+      for (i in 2:length(encoding_names)) {
+        encoded_columns <- colnames( recorded_encodings[[i]] )
+        # the factor name always comes first, so the join is always on names other 
+        # than the first name
+        encoded_columns <- encoded_columns[ 2:length(encoded_columns) ]
+        res <- recorded_encodings[[i]][res, on = encoded_columns ]
+        all_encoded_names <- c( all_encoded_names, encoded_columns)
+      }
+    }
+    res[, (all_encoded_names) := NULL]
+    
+    return( res[, .SD , .SDcols = original_colnames_X] )
+  }
+  
+  
   return( list( encoded = encoder_fit(X),
-                fitted_encoder = encoder_fit))
+                fitted_encoder = encoder_fit,
+                fitted_deencoder = encoder_reverse))
 }
